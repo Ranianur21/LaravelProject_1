d976a5df074f8bd0ac4c950822e7df5a98549bf0
Rania Nurbaity <ranianur21@gmail.com>
2025-05-19 02:31:58 +0700

    ubah sedikit

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('/posts/{post:slug}', function(Post $post) {
 });
 
 Route::get('/authors/{user:username}', function(User $user) {
-    // $posts = $user->posts->load('category', 'author');
+    // $posts =add $user->posts->load('category', 'author');
 
     return view('posts', ['title' => count($user->posts) . ' Articles by '.$user->name, 'posts' => $user->posts]);
 });
